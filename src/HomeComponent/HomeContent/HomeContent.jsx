fa9ca241bb8d514e104b71776f3b9764c7b067a6
devjihad <mdhajihad609@gmail.com>
2026-03-14 12:49:55 +0600

    running

--- src/HomeComponent/HomeContent/HomeContent.jsx
+++ src/HomeComponent/HomeContent/HomeContent.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const HomeContent = () => {
   return (
     <div>
-      fgdfgd
+     <div className='relative mt-5'>
+      <div className='w-295.25 h-75 bg-blue2 rounded-t-lg'></div>
+      <div className='w-38 h-38 bg-blue1 absolute rounded-full border-[12px] border-white top-56 left-130'></div>
+     </div>
     </div>
   )
 }
